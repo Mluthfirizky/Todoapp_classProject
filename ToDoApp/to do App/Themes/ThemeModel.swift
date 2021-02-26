1d1373a684cd2ef7d0313dbdf31b38300601f4cd
Mluthfirizky <68719514+Mluthfirizky@users.noreply.github.com>
2021-02-26 11:11:55 +0700

    Add files via upload

---
+++ ToDoApp/to do App/Themes/ThemeModel.swift
@@ -0,0 +1,14 @@
+//
+//  ThemeModel.swift
+//  Todo App
+//
+//  Created by m.luthfi rizky ginting on 15/02/21.
+//
+
+import SwiftUI
+
+struct Theme: Identifiable {
+    let id: Int
+    let themeName: String
+    let themeColor:Color
+}
